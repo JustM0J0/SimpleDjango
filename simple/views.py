d48ea891416f78957b13527653bfa39e463b029c
Mojo Jojo <63187007+JustM0J0@users.noreply.github.com>
2025-01-13 10:46:14 +0300

    Update views.py

--- simple/views.py
+++ simple/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from datetime import datetime
 
-def index(request):
+def hello(request):
     greeting = "Hello, World!"
     created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     html = f"<h1>{greeting}</h1><p>Created at: {created_at}</p>"
